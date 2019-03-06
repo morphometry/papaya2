c1d8c4b7369659b30ca18abe90657b6e5b8b7cb9
Sebastian Kapfer <sebastian.kapfer@fau.de>
2019-03-06 22:45:50 +0100

    add a regression test for PNG input and image analysis.

--- imganalysis.cpp
+++ imganalysis.cpp
@@ -4,69 +4,12 @@
 // this program considers only the red color channel.
 // 2019 Sebastian Kapfer <sebastian.kapfer@fau.de>
 #include "papaya2.hpp"
-#include "picopng.hpp"
 #include "readarg.hpp"
 
+// pull in loading of PNG image files.
+#include "pingfile.hpp"
+
 using namespace papaya2;
-
-using bytevec_t = std::vector<unsigned char>;
-
-static void load_file(bytevec_t *buffer, string const &filename);
-
-struct PingFile
-{
-    unsigned long width_, height_;
-    bytevec_t buffer_;
-
-    enum
-    {
-        RED,
-        GREEN,
-        BLUE,
-        ALPHA
-    };
-
-    PingFile(const string &infilename)
-    {
-        bytevec_t png_file_data;
-        load_file(&png_file_data, infilename);
-        if (!png_file_data.size())
-            throw std::runtime_error("file is empty");
-        int error = decodePNG(buffer_, width_, height_, png_file_data.data(),
-                              png_file_data.size(), true);
-        if (error != 0)
-            throw std::runtime_error("picopng error " + std::to_string(error));
-        if (buffer_.size() != width_ * height_ * 4)
-            throw std::runtime_error("picopng didn't decode right");
-    }
-
-    int operator()(int i, int j) const
-    {
-        if (i == -1 || j == -1)
-            return 0;
-        if (i == width() || j == height())
-            return 0;
-        return const_cast<PingFile *>(this)->at(i, j, RED);
-    }
-
-    unsigned char &at(int i, int j, int c)
-    {
-        if (!(i >= 0 && i < width()))
-            throw std::range_error("PingFile i");
-        if (!(j >= 0 && j < height()))
-            throw std::range_error("PingFile j");
-        if (!(c >= 0 && c < 4))
-            throw std::range_error("PingFile c");
-        j = height() - 1 - j;
-        int index = (i + j * width()) * 4 + c;
-        return buffer_.at(index);
-    }
-
-    int width() const { return width_; }
-    int height() const { return height_; }
-    double pixel_width() const { return 1.; }
-    double pixel_height() const { return 1.; }
-};
 
 int main(int argc, const char **argv)
 {
@@ -128,24 +71,4 @@ int main(int argc, const char **argv)
     }
 
     return 0;
-}
-
-void load_file(bytevec_t *buffer, const string &filename)
-{
-    std::ifstream file(filename.c_str(),
-                       std::ios::in | std::ios::binary | std::ios::ate);
-
-    // get filesize
-    std::streamsize size = 0;
-    if (file.seekg(0, std::ios::end).good())
-        size = file.tellg();
-    if (file.seekg(0, std::ios::beg).good())
-        size -= file.tellg();
-
-    // read contents of the file into the vector
-    if (size > 0) {
-        buffer->resize((size_t)size);
-        file.read((char *)buffer->data(), size);
-    } else
-        buffer->clear();
 }
---
+++ pingfile.hpp
@@ -0,0 +1,92 @@
+#pragma once
+
+// example of how to write a class suitable for the PHOTO parameter
+// of the template functions in namespace papaya2.
+// this example loads the red color channel from a PNG file.
+// it is used in imganalysis.cpp.
+
+#include "picopng.hpp"
+
+namespace
+{
+
+using bytevec_t = std::vector<unsigned char>;
+using string = std::string;
+
+static
+void load_file(bytevec_t *buffer, const string &filename)
+{
+    std::ifstream file(filename.c_str(),
+                       std::ios::in | std::ios::binary | std::ios::ate);
+
+    // get filesize
+    std::streamsize size = 0;
+    if (file.seekg(0, std::ios::end).good())
+        size = file.tellg();
+    if (file.seekg(0, std::ios::beg).good())
+        size -= file.tellg();
+
+    // read contents of the file into the vector
+    if (size > 0) {
+        buffer->resize((size_t)size);
+        file.read((char *)buffer->data(), size);
+    } else
+        buffer->clear();
+}
+
+struct PingFile
+{
+    unsigned long width_, height_;
+    bytevec_t buffer_;
+
+    enum
+    {
+        RED,
+        GREEN,
+        BLUE,
+        ALPHA
+    };
+
+    PingFile(const string &infilename)
+    {
+        bytevec_t png_file_data;
+        load_file(&png_file_data, infilename);
+        if (!png_file_data.size())
+            throw std::runtime_error("file is empty");
+        int error = decodePNG(buffer_, width_, height_, png_file_data.data(),
+                              png_file_data.size(), true);
+        if (error != 0)
+            throw std::runtime_error("picopng error " + std::to_string(error));
+        if (buffer_.size() != width_ * height_ * 4)
+            throw std::runtime_error("picopng didn't decode right");
+    }
+
+    int operator()(int i, int j) const
+    {
+        if (i == -1 || j == -1)
+            return 0;
+        if (i == width() || j == height())
+            return 0;
+        return const_cast<PingFile *>(this)->at(i, j, RED);
+    }
+
+    unsigned char &at(int i, int j, int c)
+    {
+        if (!(i >= 0 && i < width()))
+            throw std::range_error("PingFile i");
+        if (!(j >= 0 && j < height()))
+            throw std::range_error("PingFile j");
+        if (!(c >= 0 && c < 4))
+            throw std::range_error("PingFile c");
+        j = height() - 1 - j;
+        int index = (i + j * width()) * 4 + c;
+        return buffer_.at(index);
+    }
+
+    int width() const { return width_; }
+    int height() const { return height_; }
+    double pixel_width() const { return 1.; }
+    double pixel_height() const { return 1.; }
+};
+
+}
