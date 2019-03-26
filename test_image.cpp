428828b992cb15aff3dc597bd5335eb4c55f5929
Sebastian Kapfer <sebastian.kapfer@fau.de>
2019-03-26 14:13:06 +0100

    test regular marching squares more carefully

--- test_image.cpp
+++ test_image.cpp
@@ -122,7 +122,7 @@ TEST_CASE("marching squares algorithm")
             CHECK(ph.width() == 2);
             CHECK(ph.height() == 2);
             auto i_imt = imt_interpolated_marching_squares(ph, .5);
-            auto r_imt = imt_regular_marching_squares(ph, .5);
+            auto r_imt = imt_regular_marching_squares(ph, .75);
             CHECK(i_imt.area() == ref_volume[i] * TP_PIXEL_AREA);
             CHECK(r_imt.area() == ref_volume[i] * TP_PIXEL_AREA);
             CHECK_NOTHROW(check_msq_positively_oriented(ph, CONNECT_WHITE));
@@ -133,7 +133,7 @@ TEST_CASE("marching squares algorithm")
                   TP_PIXEL_AREA * ref_volume[i]);
             CHECK(b_imt.imt(0) == +r_imt.imt(0));
             CHECK(b_imt.imt(3) == -r_imt.imt(3));
-            b_imt = imt_regular_marching_squares(ph, .5, ANALYZE_BLACK);
+            b_imt = imt_regular_marching_squares(ph, .75, ANALYZE_BLACK);
             CHECK(TP_PIXEL_AREA - b_imt.area() ==
                   TP_PIXEL_AREA * ref_volume[i]);
             CHECK(b_imt.imt(0) == +r_imt.imt(0));
@@ -224,7 +224,7 @@ TEST_CASE("simple marching squares IMT examples")
         auto const ref_psi3 = (0.) * TP_PIXEL_SIDE;
         auto const ref_psi4 = (-2 * SQRT2) * TP_PIXEL_SIDE;
         auto const i_imt = imt_interpolated_marching_squares(one_pixel, .5);
-        auto const r_imt = imt_regular_marching_squares(one_pixel, .5);
+        auto const r_imt = imt_regular_marching_squares(one_pixel, .75);
         CHECK(i_imt.area() == approx(ref_area));
         CHECK(i_imt.msm(2) == approx(0.));
         CHECK(i_imt.msm(3) == approx(0.));
@@ -259,7 +259,7 @@ TEST_CASE("simple marching squares IMT examples")
                            " x  _"
                            "    _";
             auto i_imt = imt_interpolated_marching_squares(ph, .5);
-            auto r_imt = imt_regular_marching_squares(ph, .5);
+            auto r_imt = imt_regular_marching_squares(ph, .75);
             CHECK(i_imt.area() == approx(ref_area));
             CHECK(i_imt.perimeter() == approx(ref_peri));
             CHECK(i_imt.imt(0) == approx(ref_peri));
@@ -273,7 +273,7 @@ TEST_CASE("simple marching squares IMT examples")
             CHECK(r_imt.imt(3) == approx(ref_psi3));
             CHECK(r_imt.imt(4) == approx(ref_psi4));
             i_imt = imt_interpolated_marching_squares(ph, .5, ANALYZE_BLACK);
-            r_imt = imt_regular_marching_squares(ph, .5, ANALYZE_BLACK);
+            r_imt = imt_regular_marching_squares(ph, .75, ANALYZE_BLACK);
             CHECK(i_imt.area() == approx(12 * TP_PIXEL_AREA - ref_area));
             CHECK(i_imt.perimeter() == approx(ref_peri));
             CHECK(i_imt.imt(0) == approx(ref_peri));
@@ -298,7 +298,7 @@ TEST_CASE("simple marching squares IMT examples")
                                " xxx _"
                                "     _";
                 auto i_imt = imt_interpolated_marching_squares(ph, .5);
-                auto r_imt = imt_regular_marching_squares(ph, .5);
+                auto r_imt = imt_regular_marching_squares(ph, .75);
                 CHECK(i_imt.area() == approx(ref_area));
                 CHECK(i_imt.perimeter() == approx(ref_peri));
                 CHECK(i_imt.imt(2) == approx(ref_psi2));
@@ -311,7 +311,7 @@ TEST_CASE("simple marching squares IMT examples")
                 CHECK(r_imt.imt(4) == approx(ref_psi4));
                 i_imt =
                     imt_interpolated_marching_squares(ph, .5, ANALYZE_BLACK);
-                r_imt = imt_regular_marching_squares(ph, .5, ANALYZE_BLACK);
+                r_imt = imt_regular_marching_squares(ph, .75, ANALYZE_BLACK);
                 CHECK(i_imt.area() == approx(12 * TP_PIXEL_AREA - ref_area));
                 CHECK(i_imt.perimeter() == approx(ref_peri));
                 CHECK(i_imt.imt(0) == approx(ref_peri));
@@ -329,7 +329,7 @@ TEST_CASE("simple marching squares IMT examples")
                                "xxx _";
                 auto const ph = make_padded_view(te, 0.);
                 auto const i_imt = imt_interpolated_marching_squares(ph, .5);
-                auto const r_imt = imt_regular_marching_squares(ph, .5);
+                auto const r_imt = imt_regular_marching_squares(ph, .75);
                 CHECK(i_imt.area() == approx(ref_area));
                 CHECK(i_imt.perimeter() == approx(ref_peri));
                 CHECK(i_imt.imt(2) == approx(ref_psi2));
@@ -348,7 +348,7 @@ TEST_CASE("simple marching squares IMT examples")
                                " xxx_";
                 auto const ph = make_padded_view(te, 0.);
                 auto const i_imt = imt_interpolated_marching_squares(ph, .5);
-                auto const r_imt = imt_regular_marching_squares(ph, .5);
+                auto const r_imt = imt_regular_marching_squares(ph, .75);
                 CHECK(i_imt.area() == approx(ref_area));
                 CHECK(i_imt.perimeter() == approx(ref_peri));
                 CHECK(i_imt.imt(2) == approx(ref_psi2));
