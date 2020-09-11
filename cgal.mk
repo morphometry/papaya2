3cca0861fc985aae3de4d190f6fa049c1e0bffb4
Sebastian Kapfer <sebastian.kapfer@fau.de>
2020-09-11 10:50:47 +0200

    fix typo

--- cgal.mk
+++ cgal.mk
@@ -1,4 +1,4 @@
 CGAL_CXXFLAGS = -DHAVE_CGAL
 # for CGAL 4.9 and above, you can reduce the following line to
 # CGAL_LDFLAGS = -lgmp
-CGAL_LDFLAGS = -lCGAL -LCGAL_Core -lgmp
+CGAL_LDFLAGS = -lCGAL -lCGAL_Core -lgmp
