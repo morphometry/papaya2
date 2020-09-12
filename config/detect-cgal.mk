1da33cfd5c298eb418c423ef32193c2c00198823
Sebastian Kapfer <sebastian.kapfer@appfolio.com>
2020-09-12 14:55:04 -0700

    fix warning during CGAL detection

--- config/detect-cgal.mk
+++ config/detect-cgal.mk
@@ -1,8 +1,10 @@
 PRODUCTS += .cgal.mk
 MAKEFILES += .cgal.mk
-.cgal.mk:
-	@echo Please ignore the above error about .cgal.mk not existing.
-	@($(CXX) $(CXXFLAGS) -std=c++14 -o /dev/null -c ../config/test-recent-cgal.cpp 2>/dev/null && echo Detected CGAL 5.0 and above. && cp ../config/recent-cgal.mk .cgal.mk) || \
-            ($(CXX) $(CXXFLAGS) -o /dev/null -c ../config/test-any-cgal.cpp 2>/dev/null && echo Detected CGAL prior to 5.0. && cp ../config/older-cgal.mk .cgal.mk) || \
-            (echo Found no CGAL headers. && cp ../config/no-cgal.mk .cgal.mk)
+
+CGD_TEST_COMPILE = $(CXX) $(CXXFLAGS) -o/dev/null 2>/dev/null -c
+DUMMY_VARIABLE := $(shell [ -r .cgal.mk ] || \
+    ($(CGD_TEST_COMPILE) -std=c++14 ../config/test-recent-cgal.cpp && echo Detected CGAL 5.0 and above. >&2 && cp ../config/recent-cgal.mk .cgal.mk) || \
+    ($(CGD_TEST_COMPILE) ../config/test-any-cgal.cpp && echo Detected CGAL prior to 5.0. >&2 && cp ../config/older-cgal.mk .cgal.mk) || \
+    (echo Found no CGAL headers. >&2 && cp ../config/no-cgal.mk .cgal.mk) \
+)
 include .cgal.mk
