7e1d21d875d5b17773dc11356be790d3e0fd95f8
Sebastian Kapfer <sebastian.kapfer@fau.de>
2020-09-16 10:54:23 +0200

    do not overwrite variables in example

--- matlab/imganalysis.m
+++ matlab/imganalysis.m
@@ -1,5 +1,5 @@
 mex imt_for_image.cpp;
 a = imread("potato.png");
 a = a(:,:,1);
-imt = imt_for_image(a, [ 1.469734492275599e+02 ])
-imt = imt_for_image(a, [ linspace(100,150,5) ])
+imt_one_threshold = imt_for_image(a, [ 1.469734492275599e+02 ])
+imt_five_thresholds = imt_for_image(a, [ linspace(100,150,5) ])
--- matlab/ppanalysis.m
+++ matlab/ppanalysis.m
@@ -1,10 +1,11 @@
 % add -I (includepath) -L (libpath) -l (libraries) as required
+% see imt_for_pointpattern.cpp for more information
 mex -v -lCGAL -lCGAL_Core -lgmp imt_for_pointpattern.cpp
 a = readtable('test_pp.txt');
 a = table2array(a);
 % without periodic boundary conditions
-imt = imt_for_pointpattern(a)
+imt_open = imt_for_pointpattern(a)
 % with periodic boundary conditions
-imt = imt_for_pointpattern(a, [500., 500.])
+imt_pbc = imt_for_pointpattern(a, [500., 500.])
 % look at the q2 values for all the seeds
-imt(:,5)
+imt_pbc(:,5)
